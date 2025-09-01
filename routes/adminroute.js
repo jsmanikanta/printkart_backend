f7a74817bc2894865c18683dc4f13f6b7741fac2
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-09-01 18:49:33 +0530

    Update adminroute.js

--- routes/adminroute.js
+++ routes/adminroute.js
@@ -2,10 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getAllOrders, getOrderById } = require("../controllers/admin");
 const { verifyToken, authorizeAdmin } = require("../verifyToken");
-<<<<<<< HEAD
-=======
 
->>>>>>> 79839eb14069558f997d514a89fda300ac555384
 router.get("/printorders", verifyToken, authorizeAdmin, getAllOrders);
 router.get("/printorders/:orderId", verifyToken, authorizeAdmin, getOrderById);
 
