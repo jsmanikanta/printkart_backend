1bc22fe208208c6484008e8f6b3a38bda282bfb0
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-10-18 13:21:33 +0530

    Update ordersroute.js

--- routes/ordersroute.js
+++ routes/ordersroute.js
@@ -5,7 +5,14 @@ const router = express.Router();
 const { verifyToken } = require("../verifyToken");
 const { orderPrint } = require("../controllers/orderprints");
 const upload = multer({ storage: multer.memoryStorage() });
-router.post("/orderprints", verifyToken, upload.single("file"), orderPrint);
 
-
+router.post(
+  "/orderprints",
+  verifyToken,
+  upload.fields([
+    { name: "file", maxCount: 1 },
+    { name: "transctionid", maxCount: 1 },
+  ]),
+  orderPrint
+);
 module.exports = router;
