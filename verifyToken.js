63ec368c45d6e2af12a4649a25ca69176ae9ee17
J S Manikanta <jsmanikanta13@gmail.com>
2025-10-20 10:02:55 +0530

    statusupdat4467

--- verifyToken.js
+++ verifyToken.js
@@ -1,43 +1,37 @@
 const User = require("./models/user");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const secretkey = process.env.secretkey;
 
 const verifyToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token =
-    req.headers.token ||
-    (req.headers.authorization && req.headers.authorization.split(" ")[1]);
+  const token = req.headers.token || (authHeader && authHeader.split(" ")[1]);
 
-  if (!token) return res.status(401).json({ error: "Token is required" });
+  if (!token) {
+    return res.status(401).json({ error: "Token is required" });
+  }
 
   try {
-    const decoded = jwt.verify(token, process.env.secretkey);
-    req.user = decoded;
+    const secretkey = process.env.SECRETKEY;
+    const decoded = jwt.verify(token, secretkey);
 
     const user = await User.findById(decoded.userId);
-    if (!user) return res.status(404).json({ error: "User not found" });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
     req.userId = user._id;
     req.user = user;
     next();
   } catch (error) {
-    if (error.name === "TokenExpiredError")
+    if (error.name === "TokenExpiredError") {
       return res
         .status(401)
         .json({ error: "Token expired, please login again" });
+    }
     return res.status(403).json({ error: "Invalid token" });
   }
 };
 
-const authorizeAdmin = (req, res, next) => {
-  if (!req.user || req.user.role !== "admin") {
-    return res.status(403).json({ error: "Access denied. Admins only." });
-  }
-  next();
-};
-
 module.exports = {
   verifyToken,
-  authorizeAdmin,
 };
