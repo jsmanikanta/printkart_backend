3314c878398bc7c5f6f15ddc78c3592b484ede34
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-11-19 09:48:12 +0530

    Update prints.js

--- models/prints.js
+++ models/prints.js
@@ -23,11 +23,6 @@ const printSchema = new mongoose.Schema({
   userid: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
   transctionid: { type: String },
   payment: {type: String ,required: true},
-  status: {
-    type: String,
-    enum: [" ordered", "dispatched", "out for delivery", "delivered"],
-    default: "ordered",
-  },
   orderDate: { type: Date, default: Date.now },
   status: {
     type: String,
