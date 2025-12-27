1372852cbacd25ec5a59623bf4843fa6f2ee2de8
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-12-27 15:46:50 +0530

    Update coupon.js

--- models/coupon.js
+++ models/coupon.js
@@ -1,21 +1,23 @@
+// models/coupon.js (or Couponstatus.js)
 const mongoose = require("mongoose");
+
 const couponStatusSchema = new mongoose.Schema({
   userid: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "user",
     required: true,
   },
-
-  status: { type: Boolean, default: false }, 
+  status: { type: Boolean, default: false },
   code: { type: String, required: true },
   discountPercentage: { type: Number, required: true },
   usedDate: { type: Date, default: null },
+
+  // store user details when coupon is used
   userName: { type: String },
   userEmail: { type: String },
+  userMobile: { type: String },
 });
 
 const Couponstatus = mongoose.model("Couponstatus", couponStatusSchema);
 
 module.exports = Couponstatus;
-
-
