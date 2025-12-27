ed2f788ee1123fd0b0c8a3aaed2c21053da1c200
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2025-12-27 12:32:29 +0530

    Update papersroute.js

--- routes/papersroute.js
+++ routes/papersroute.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { getPreviousYears } = require('../controllers/previous'); 
-
+const { verifyToken } = require("../verifyToken");
 const router = express.Router();
 
-router.get('/previous-years', getPreviousYears);
+router.get('/previous-years', getPreviousYears,verifyToken);
 
 module.exports = router;
