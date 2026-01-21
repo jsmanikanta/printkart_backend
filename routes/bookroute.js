b9d87e3218ec692b1af22280616020c80691ead0
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2026-01-21 20:47:49 +0530

    Update bookroute.js

--- routes/bookroute.js
+++ routes/bookroute.js
@@ -1,13 +1,11 @@
+module.exports = router;
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
-
 const {
   Sellbook,
 } = require("../controllers/bookscontroller");
-
 const { verifyToken } = require("../verifyToken");
-
 router.post("/sellbook", verifyToken, upload.single("image"), Sellbook);
 module.exports = router;
