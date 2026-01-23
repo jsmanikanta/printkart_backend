f0545140b592615eb00f133d69ef7abd412f0721
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2026-01-23 18:58:58 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const userschema = new mongoose.Schema({
-  fullname: { type: String, required: true },
-  mobileNumber: { type: String, required: true },
+  fullname: { type: String, required: true,unique: true },
+  mobileNumber: { type: String, required: true,unique: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  birthday:{type: Date},
+  location:{type:String},
+  userType:{type: String,
+            enum:["vendor","user"],
+           }
 });
 
 const User = mongoose.model("user", userschema);
