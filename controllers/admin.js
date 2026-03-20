8e82c73b459c93f506b96ceda996821e09374387
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2026-03-20 21:53:56 +0530

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cloudinary = require("cloudinary");
-const streamifier = require("streamifier");
-const PrintsImport = require("../models/prints");
-const SellbooksImport = require("../models/sellbooks");
-const BookCategoryImageImport = require("../models/categeory");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cloudinary from "cloudinary";
+import streamifier from "streamifier";
+
+import PrintsImport from "../models/prints.js";
+import SellbooksImport from "../models/sellbooks.js";
+import BookCategoryImageImport from "../models/categeory.js";
 
 const Prints = PrintsImport.default || PrintsImport;
 const Sellbooks = SellbooksImport.default || SellbooksImport;
