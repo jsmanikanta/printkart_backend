5158df273dc5a6347bf4c743ecafd1825ab186ed
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2026-03-21 17:38:10 +0530

    Update bookroute.js

--- routes/bookroute.js
+++ routes/bookroute.js
@@ -1,9 +1,9 @@
-import express from "express";
-import multer from "multer";
-
+const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const upload = multer({ storage: multer.memoryStorage() });
+
 const {
   Sellbook,
   updateSoldStatus,
